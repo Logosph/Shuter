fc41e7c105dcd4fa74c6c8b0565eda2b5567b3fc
Daniil <zaklyakov2004@ya.ru>
2020-10-17 12:03:09 +0300

    pause button

---
+++ Shooter/Assets/Scripts/pause.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pause : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3((float)(Screen.width * 0.05), (float)(Screen.height * 0.9), 0.0f);
+    }
+
+    public void pauseButton()
+    {
+        if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+        }
+        else
+        {
+            Time.timeScale = 0;
+        }
+
+    }
+}
